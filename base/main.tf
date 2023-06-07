689d4fc9d69135e9c2e9334a3c6da6b7feac8ab3
Marc Leibold <leibold485@gmail.com>
2023-06-07 15:29:04 +0200

    Use Helm Chart for Container Registry (with UI) (#38)

--- base/main.tf
+++ base/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.8"
 
   required_providers {
-    kubernetes  = "~> 2.17"
+    kubernetes = "~> 2.17"
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
@@ -11,195 +11,207 @@ terraform {
 }
 
 provider "kubernetes" {
-    config_path     = "~/.kube/config"
-    config_context  = "spof"
+  config_path    = "~/.kube/config"
+  config_context = "spof"
 }
 
 provider "kubectl" {
-    config_path     = "~/.kube/config"
-    config_context  = "spof"
+  config_path    = "~/.kube/config"
+  config_context = "spof"
 }
 
 provider "helm" {
-    kubernetes {
-        config_path     = "~/.kube/config"
-        config_context  = "spof"
-    }
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "spof"
+  }
 }
 
 # cert-manager
 data "kubectl_path_documents" "cert-manager_manifests" {
-    pattern = "manifests/cert-manager/*.yaml"
+  pattern = "manifests/cert-manager/*.yaml"
 }
 
 resource "kubectl_manifest" "cert-manager" {
-    count = length(data.kubectl_path_documents.cert-manager_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.cert-manager_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.cert-manager_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.cert-manager_manifests.documents, count.index)
 }
 
 # nginx ingress
 data "kubectl_path_documents" "nginx-ingress_manifests" {
-    pattern = "manifests/ingress-controller/*.yaml"
+  pattern = "manifests/ingress-controller/*.yaml"
 }
 
 resource "kubectl_manifest" "nginx-ingress" {
-    depends_on = [
-        kubectl_manifest.cert-manager
-    ]
+  depends_on = [
+    kubectl_manifest.cert-manager
+  ]
 
-    count = length(data.kubectl_path_documents.nginx-ingress_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.nginx-ingress_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.nginx-ingress_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.nginx-ingress_manifests.documents, count.index)
 }
 
 # User for GitHub Actions
 data "kubectl_path_documents" "github-actions-user_manifests" {
-    pattern = "manifests/gh-actions-user/*.yaml"
+  pattern = "manifests/gh-actions-user/*.yaml"
 }
 
 resource "kubectl_manifest" "github-actions-user" {
-    count = length(data.kubectl_path_documents.github-actions-user_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.github-actions-user_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.github-actions-user_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.github-actions-user_manifests.documents, count.index)
 }
 
 # NFS Provisioner
 data "kubectl_path_documents" "nfs-provisioner_manifests" {
-    pattern = "manifests/nfs-provisioner/*.yaml"
+  pattern = "manifests/nfs-provisioner/*.yaml"
 }
 
 resource "kubectl_manifest" "nfs-provisioner" {
-    count = length(data.kubectl_path_documents.nfs-provisioner_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.nfs-provisioner_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.nfs-provisioner_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.nfs-provisioner_manifests.documents, count.index)
 }
 
 # Postgres DB
 data "kubectl_path_documents" "postgres-db_manifests" {
-    pattern = "manifests/postgres-db/*.yaml"
-    vars = {
-        POSTGRES_PASSWORD           = base64encode(var.postgres_settings.admin_password)
-        NEXTCLOUD_DB_PASSWORD       = var.nextcloud_settings.db_password
-        HOMEASSISTANT_DB_PASSWORD   = var.homeassistant_settings.db_password
-    }
+  pattern = "manifests/postgres-db/*.yaml"
+  vars = {
+    POSTGRES_PASSWORD         = base64encode(var.postgres_settings.admin_password)
+    NEXTCLOUD_DB_PASSWORD     = var.nextcloud_settings.db_password
+    HOMEASSISTANT_DB_PASSWORD = var.homeassistant_settings.db_password
+  }
 }
 
 resource "kubectl_manifest" "postgres-db" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner
-    ]
+  depends_on = [
+    kubectl_manifest.nfs-provisioner
+  ]
 
-    count = length(data.kubectl_path_documents.postgres-db_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.postgres-db_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.postgres-db_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.postgres-db_manifests.documents, count.index)
 }
 
 # container registry
-data "kubectl_path_documents" "container-registry_manifests" {
-    pattern = "manifests/container-registry/*.yaml"
+data "kubectl_path_documents" "container-registry-data_manifests" {
+  pattern = "manifests/container-registry-data/*.yaml"
 
-    vars = {
-        REGISTRY_USERNAME = var.registry_settings.username
-        REGISTRY_HTPASSWD = var.registry_settings.htpasswd
-    }
+  vars = {
+    REGISTRY_USERNAME = var.registry_settings.username
+    REGISTRY_HTPASSWD = var.registry_settings.htpasswd
+  }
 }
 
-resource "kubectl_manifest" "container-registry" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner,
-        kubectl_manifest.nginx-ingress
-    ]
+resource "kubectl_manifest" "container-registry-data" {
+  depends_on = [
+    kubectl_manifest.nfs-provisioner
+  ]
 
-    count = length(data.kubectl_path_documents.container-registry_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.container-registry_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.container-registry-data_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.container-registry-data_manifests.documents, count.index)
+}
+
+resource "helm_release" "container-registry" {
+  name       = "container-registry"
+  chart      = "docker-registry-ui"
+  repository = "https://helm.joxit.dev"
+  version    = "1.0.1"
+  timeout    = 6000
+  wait       = true
+
+  values = [
+    file("manifests/container-registry/values.yaml")
+  ]
 }
 
 # home-assistant
 data "kubectl_path_documents" "home-assistant_manifests" {
-    pattern = "manifests/home-assistant/*.yaml"
-    vars = {
-        HOMEASSISTANT_DB_PASSWORD = var.homeassistant_settings.db_password
-    }
+  pattern = "manifests/home-assistant/*.yaml"
+  vars = {
+    HOMEASSISTANT_DB_PASSWORD = var.homeassistant_settings.db_password
+  }
 }
 
 resource "kubectl_manifest" "home-assistant" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner,
-        kubectl_manifest.nginx-ingress
-    ]
+  depends_on = [
+    kubectl_manifest.nfs-provisioner,
+    kubectl_manifest.nginx-ingress
+  ]
 
-    count = length(data.kubectl_path_documents.home-assistant_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.home-assistant_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.home-assistant_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.home-assistant_manifests.documents, count.index)
 }
 
 # esphome
 data "kubectl_path_documents" "esphome_manifests" {
-    pattern = "manifests/esphome/*.yaml"
+  pattern = "manifests/esphome/*.yaml"
 }
 
 resource "kubectl_manifest" "esphome" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner,
-        kubectl_manifest.nginx-ingress,
-        kubectl_manifest.home-assistant
-    ]
+  depends_on = [
+    kubectl_manifest.nfs-provisioner,
+    kubectl_manifest.nginx-ingress,
+    kubectl_manifest.home-assistant
+  ]
 
-    count = length(data.kubectl_path_documents.esphome_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.esphome_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.esphome_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.esphome_manifests.documents, count.index)
 }
 
 # grafana
 data "kubectl_path_documents" "grafana_manifests" {
-    pattern = "manifests/grafana/*.yaml"
+  pattern = "manifests/grafana/*.yaml"
 }
 
 resource "kubectl_manifest" "grafana" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner,
-        kubectl_manifest.nginx-ingress
-    ]
+  depends_on = [
+    kubectl_manifest.nfs-provisioner,
+    kubectl_manifest.nginx-ingress
+  ]
 
-    count = length(data.kubectl_path_documents.grafana_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.grafana_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.grafana_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.grafana_manifests.documents, count.index)
 }
 
 # Kubernetes API ingress
 data "kubectl_path_documents" "kubernetes-api-ingress_manifests" {
-    pattern = "manifests/kubernetes-api-ingress/*.yaml"
+  pattern = "manifests/kubernetes-api-ingress/*.yaml"
 }
 
 resource "kubectl_manifest" "kubernetes-api-ingress" {
-    depends_on = [
-        kubectl_manifest.nginx-ingress
-    ]
+  depends_on = [
+    kubectl_manifest.nginx-ingress
+  ]
 
-    count = length(data.kubectl_path_documents.kubernetes-api-ingress_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.kubernetes-api-ingress_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.kubernetes-api-ingress_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.kubernetes-api-ingress_manifests.documents, count.index)
 }
 
 # Kubernetes Dashboard
 data "kubectl_path_documents" "kubernetes-dashboard_manifests" {
-    pattern = "manifests/kubernetes-dashboard/*.yaml"
+  pattern = "manifests/kubernetes-dashboard/*.yaml"
 }
 
 resource "kubectl_manifest" "kubernetes-dashboard" {
-    depends_on = [
-        kubectl_manifest.nginx-ingress
-    ]
+  depends_on = [
+    kubectl_manifest.nginx-ingress
+  ]
 
-    count = length(data.kubectl_path_documents.kubernetes-dashboard_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.kubernetes-dashboard_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.kubernetes-dashboard_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.kubernetes-dashboard_manifests.documents, count.index)
 }
 
 # NTFY Server
 data "kubectl_path_documents" "ntfy-server_manifests" {
-    pattern = "manifests/ntfy/*.yaml"
+  pattern = "manifests/ntfy/*.yaml"
 }
 
 resource "kubectl_manifest" "ntfy-server" {
-    depends_on = [
-        kubectl_manifest.nfs-provisioner,
-        kubectl_manifest.nginx-ingress
-    ]
+  depends_on = [
+    kubectl_manifest.nfs-provisioner,
+    kubectl_manifest.nginx-ingress
+  ]
 
-    count = length(data.kubectl_path_documents.ntfy-server_manifests.documents)
-    yaml_body = element(data.kubectl_path_documents.ntfy-server_manifests.documents, count.index)
+  count     = length(data.kubectl_path_documents.ntfy-server_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.ntfy-server_manifests.documents, count.index)
 }
 
 # Mosquitto
--- base/variables.tf
+++ base/variables.tf
@@ -1,24 +1,24 @@
 variable "homeassistant_settings" {
-    type = object({
-        db_password = string
-    })
+  type = object({
+    db_password = string
+  })
 }
 variable "nextcloud_settings" {
-    type = object({
-        admin_password  = string
-        db_password     = string
-    })
+  type = object({
+    admin_password = string
+    db_password    = string
+  })
 }
 
 variable "postgres_settings" {
-    type = object({
-        admin_password = string
-    })
+  type = object({
+    admin_password = string
+  })
 }
 
 variable "registry_settings" {
-    type = object({
-        username = string
-        htpasswd = string
-    })
+  type = object({
+    username = string
+    htpasswd = string
+  })
 }
